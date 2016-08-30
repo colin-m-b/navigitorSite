95fa5e357edb9c66cb44dc17aeea218c420324d4
steve <stevenharlan@gmail.com>
2016-08-29 22:43:22 -0700

    debugging

--- server.js
+++ server.js
@@ -21,39 +21,33 @@ if (PORT === process.env.PORT) {
 console.log('Polling server is running on http://localhost:' + PORT);
 
 
-/*************
-*** Socket Handling ***
-**************/
-var connectedClients = 0;
+/***************************
+*** Socket Handling + RxJS ***
+TODO: handle subscribe/getRepo functionality on client side
+****************************/
+
 io.sockets.on('connection', function (socket) {
-  connectedClients++;
-  console.log(`${connectedClients} are Connected on socket server`);
   // room handling
-  socket.on('subscribe', function(data) { socket.join(data.room); console.log(`joined room:${data.room}`)})
-  socket.on('unsubscribe', function(data) { socket.leave(data.room); console.log(`left room:${data.room}`) })
+  socket.on('subscribe', function(data) {
+    // EventController.getRepo(data, function(x) {
+    //   console.log(x)
+    // })
+    socket.join(data.room)}
+  );
+  socket.on('unsubscribe', function(data) { socket.leave(data.room)});
   // Socket test
   socket.once("echo", function (msg, callback) {
     socket.emit("echo", msg);
   });
-  //listening for commit from local client, then broadcasts to all connected clients
-  socket.on('broadcastCommit', function(arg){
-		console.log('broadcastCommit: ' + arg);
+  //listening for Git Action from local client, then broadcasts to all connected clients in team
+	// TODO: handle callback in post method
+	socket.on('broadcastGit', function(arg){
+    // EventController.post(arg, function(data) {
+		// 	console.log(data);
+    // });
 		io.in(arg.room).emit('incomingCommit', arg.data);
 	});
-  // listening for branch change from local client, then broadcasts to all connected clients
-	socket.on('broadcastBranch', function(arg){
-		console.log('Branch server event: ' + arg);
-		io.in(arg.room).emit('incomingCommit', arg.data)
-  });
-  socket.on('disconnect', function(){
-    connectedClients--;
-    console.log(`Disconnection: now ${connectedClients} are Connected on socket server`);
-  })
 });
-
-
-
-
 
 /*************
 *** O Auth ***
