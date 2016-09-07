9297b64d92027d3fdf1aaca39b1c1bd8158a9284
steve <stevenharlan@gmail.com>
2016-09-07 14:49:36 -0700

    added bodyparsing1

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var bodyParser = require ('body-parser');
 var Rx = require('rxjs/Rx');
 
 app.use(express.static('public'));
-
+app.use(bodyParser.urlencoded({extended: true}))
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, './index.html'))
 });
