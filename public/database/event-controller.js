e93f2f25feb8eda5288aea3175d24e8317d2c69a
steve <stevenharlan@gmail.com>
2016-09-07 13:37:40 -0700

    added db verify26

--- public/database/event-controller.js
+++ public/database/event-controller.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 // // mongoose.connect('mongodb://ec2-54-152-1-18.compute-1.amazonaws.com');
-// var MONGO_URI = 'mongodb://navigitor:browncouch123@ds019826.mlab.com:19826/navigitor'
+var MONGO_URI = 'mongodb://navigitor:browncouch123@ds019826.mlab.com:19826/navigitor'
 // // var MONGO_URI = 'mongodb://localhost/test'
 // // var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
 // //                 replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
-// mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI);
 // mongoose.connection.on('connected', function() {console.log('event connected on mLab')})
 // mongoose.connection.on('error', function(e) {console.log('CONNECTION ERROR FROM EVENT: ' + e)})
 //
