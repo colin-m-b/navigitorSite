62c18e5ed1d434c1d35feebe3be253cd0891c1f8
steve <stevenharlan@gmail.com>
2016-09-07 12:01:08 -0700

    resest to head

--- server.js
+++ server.js
@@ -1,37 +1,16 @@
-const express = require('express');
-const app = express();
-const config = require('../webpack.config');
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-// const EventController = require('../src/database/event-controller.js')
-// const UserController = require ('../src/database/user-controller.js')
-// process.env.PORT sets to hosting service port (Heroku) or 3000
-const PORT = process.env.PORT || 3000;
-const server = app.listen(PORT);
-const io = require('socket.io').listen(server);
-const bodyParser = require ('body-parser');
-const Rx = require('rxjs/Rx');
+var express = require('express');
+var app = express();
+var path = require('path');
 
+var PORT = process.env.PORT || 3000;
+var server = app.listen(PORT);
+var io = require('socket.io').listen(server);
 
-app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, './index.html'))
 });
-
-/************************************************
-*** Development: Webpack Config/Middleware ***
-**************************************************/
-// hands this compiler off to the middleware for hot reloading
-const compiler = webpack(config);
-app.use(webpackDevMiddleware(compiler, {
-	noInfo: true,
-	// public path simulates publicPath of config file
-	publicPath: config.output.publicPath
-}));
-app.use(webpackHotMiddleware(compiler));
 
 if (PORT === process.env.PORT) {
   app.use(express.static('./'))
@@ -41,95 +20,71 @@ if (PORT === process.env.PORT) {
 
 console.log('Polling server is running on http://localhost:' + PORT);
 
+
 /***************************
 *** Socket Handling + RxJS ***
 TODO: handle subscribe/getRepo functionality on client side
 ****************************/
-io.sockets.on('connection', function(socket){
-	// Room Handling
-	const socketJoinRoomObservable = Rx.Observable.create(function(observer){
-		socket.on('subscribe', function(data) {
-			try {
-				EventController.getRepo(data, function(x) {
-					socket.emit('completeDBLog', x)
-				})
-				socket.join(data.room)
-				observer.next(data.room);
-			} catch (err) {
-				observer.error(err);
-			}
-		}
-		);
-	})
 
-	const socketJoinRoomObserver = socketJoinRoomObservable
-		.subscribe(x => console.log('joined team: ' + x), e => 'connection error: ' + e, () => console.log('team connected complete'))
-
-	const socketLeaveRoomObservable = Rx.Observable.create(function(observer){
-		socket.on('unsubscribe', function(data) {
-			try{
-				socket.leave(data.room);
-				observer.next(data.room);
-			} catch (err) {
-				observer.error(err);
-			}
-		});
+io.sockets.on('connection', function (socket) {
+  // room handling
+  socket.on('subscribe', function(data) {socket.join(data.room)});
+  socket.on('unsubscribe', function(data) { socket.leave(data.room)});
+  // Socket test
+  socket.once("echo", function (msg, callback) {
+    socket.emit("echo", msg);
+  });
+  //listening for Git Action from local client, then broadcasts to all connected clients in team
+	// TODO: handle callback in post method
+	socket.on('broadcastGit', function(arg){
+		io.in(arg.room).emit('incomingCommit', arg.data);
 	});
-
-	const socketLeaveRoomObserver = socketLeaveRoomObservable
-		.subscribe(x => console.log('left room: ' + x), e => console.log('error on leave: ' + e),() => console.log('left room completed'))
-
-	// Broadcasting Git Actions from local clients to connected team members
-	const socketGitBroadcastingObservable = Rx.Observable.create(function(observer){
-		socket.on('broadcastGit', function(arg){
-			try {
-				console.log('from server bcast git ' + arg.data)
-				EventController.saveEvent(arg);
-				io.in(arg.room).emit('incomingCommit', arg.data);
-			} catch (err) {
-				observer.error(err);
-			}
-		});
-	});
-
-	const socketGitBroadcastingObserver = socketGitBroadcastingObservable
-		.subscribe(x => console.log('broadcasted'), e => console.log(e), () => console.log('git broadcasted and saved | complete'))
-		//Chat room
-	  socket.on('sendMessage', function (data) {
-	    socket.broadcast.to(data.room).emit('sendMessage', {
-	      text: data.text
-	    });
-
-	    console.log(data.text);
-	  });
 });
 
-/***********************
- *** User Sign in/up ***
- ***********************/
-// 
-// app.post('/signup', (req, res) => {
-//   UserController.add(req, () => {
-//     console.log('hi')
-//   })
-//   //console.log('signed up')
-// })
+
+
+
+
+
+/*************
+*** O Auth ***
+**************/
+// var options = {
+//   client_id: 'INSERT CLIENT ID',
+//   client_secret: 'INSERT CLIENT SECRET'
+// }
 //
-// app.post('/verify', (req, res) => {
-//   UserController.verify(req, function(data) {
-//     console.log('data from server: ', data)
-//     res.send(data)
-//   })
-//   //console.log(req)
-// })
+// var oauth = require("oauth").OAuth2;
+// var OAuth2 = new oauth(options.client_id, options.client_secret, "https://github.com/", "login/oauth/authorize", "login/oauth/access_token");
 //
-// /*****************
-//  *** Analytics ***
-//  *****************/
+// app.get('/auth/github',function(req,res){
 //
-// app.get('/days', (req, res) => {
-//   EventController.getByTime(req, function() {
-//     res.send(data)
-//   })
+//   res.writeHead(303, {
+//     Location: OAuth2.getAuthorizeUrl({
+//       redirect_uri: 'http://localhost:3000/auth/github/callback',
+//       scope: "user,repo,gist"
+//     })
+//   });
+//   res.end();
+// });
 //
-// })
+//
+// app.get('/auth/github/callback',function (req, res) {
+//   var code = req.query.code;
+//   OAuth2.getOAuthAccessToken(code, {}, function (err, access_token, refresh_token) {
+//     if (err) {
+//       console.log(err);
+//     }
+//     accessToken = access_token;
+//     // authenticate github API
+//     console.log("AccessToken: "+accessToken+"\n");
+//     github.authenticate({
+//       type: "oauth",
+//       token: accessToken
+//     });
+//   });
+//   res.redirect('/');
+// });
+
+
+module.exports = server;
