eabda482574610b4b49285b18c7ff7a28c38e50a
steve <stevenharlan@gmail.com>
2016-09-07 12:16:53 -0700

    added db verify

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-// var EventController = require('./database/event-controller.js')
-// var UserController = require ('./src/database/user-controller.js')
+var EventController = require('./database/event-controller.js')
+var UserController = require ('./src/database/user-controller.js')
 
 var PORT = process.env.PORT || 3000;
 var server = app.listen(PORT);
