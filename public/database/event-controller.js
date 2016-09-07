4e2780d3bebaaf9d6782fd884de6f6f0bf64e923
steve <stevenharlan@gmail.com>
2016-09-07 14:08:47 -0700

    added db verify32

--- public/database/event-controller.js
+++ public/database/event-controller.js
@@ -1,15 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-// mongoose.connect('mongodb://ec2-54-152-1-18.compute-1.amazonaws.com');
 var MONGO_URI = 'mongodb://navigitor:browncouch123@ds019826.mlab.com:19826/navigitor'
 // var MONGO_URI = 'mongodb://localhost/test'
-// var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
-//                 replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
 mongoose.connect(MONGO_URI);
-mongoose.connection.on('connected', function() {console.log('event connected on mLab')})
-mongoose.connection.on('error', function(e) {console.log('CONNECTION ERROR FROM EVENT: ' + e)})
+// mongoose.connection.on('connected', function() {console.log('event connected on mLab')})
+// mongoose.connection.on('error', function(e) {console.log('CONNECTION ERROR FROM EVENT: ' + e)})
 
-mongoose.connection.once('open', function(){
 let eventSchema = new mongoose.Schema({
   user: {type: String, required: true},
   SHA: String,
@@ -27,27 +23,7 @@ EventController.saveEvent = function(arg) {
   let gitData = JSON.parse([arg.data]);
   console.log('author from db' + gitData.author)
     //create Event model using room property passed from argument as the collection name
-    let Event = mongoose.model(arg.room, eventSchema);
-    //create new instance of event
-    //let eventToAdd = new Event();
-    //check if data is coming in as array, which will only happen if a new user
-    //with prior history in Git tree joins team
-    // if (Array.isArray(arg.data)) {
-    //     //iterate through array, adding each
-    //     arg.data.forEach(elem => {
-    //         //parse user from each element in array
-    //         //eventToAdd.user = elem.data.substring(83, elem.data.indexOf('<') - 1);
-    //         //eventToAdd.time = elem.data.substring(elem.data.indexOf('>') + 1, elem.data.indexOf('>') + 12);
-    //         //eventToAdd.data = JSON.stringify(elem);
-    //
-    //         //save event to collection or create new collection
-    //         //Event.create(eventToAdd)
-    //       eventToAdd.save(function(err){
-    //         if(err) console.log('error saving in DB: ' + err)
-    //       })
-    //     })
-    // //else if a single instance of Git event
-    // } else {
+    let Event = mongoose.model(arg.room, eventSchema)
       var eventToAdd = new Event({
       user: gitData.author,
       SHA: gitData.SHA,
@@ -58,7 +34,6 @@ EventController.saveEvent = function(arg) {
       });
         console.log('newevent: ', eventToAdd)
         //save event to collection or create new collection
-        //Event.create(eventToAdd);
         eventToAdd.save(function(err){
           if(err) console.log('error saving in DB: ' + err)
         })
@@ -87,35 +62,5 @@ EventController.getByTime = (arg, callback) => {
         callback(data)
     })
 }
-
-});
-// EventController.getUser = (arg, callback) => {
-//     mongoose.connect('mongodb://localhost/test', err => {
-//         if (err) return console.error(err);
-//         let coll = mongoose.model(arg.room + 's', eventSchema)
-//         coll.findOne({'user': arg.user}, 'user data', (err, user) => {
-//             if (err) return console.error(err)
-//             callback(user);
-//             mongoose.connection.close();
-//         })
-//     })
-// }
-
-// EventController.getAllRepos = (arg, callback) => {
-//     mongoose.connect('mongodb://localhost/test', err => {
-//         if (err) return console.error(err);
-//         const collArr = mongoose.connections[0];
-//         // const allEvents = collArr.map(function(elem) {
-//         //     return db[elem].find();
-//         // });
-//         // res.send(allEvents);
-//         callback(collArr)
-//     })
-// }
-// const test = {
-//     room: 'testrepo',
-//     data: '"19cbd65fba1ba345fc927395d572830162f7b1cf 5e860081d9f7ccc7cbc0a64922beed6d6ac09de9 Colin Brownlie <colin@Colins-MacBook-Pro.local> 1472261199 -0700     \\tcommit (merge): fixing merge conflict"'
-// }
-// EventController.post(test)
 
 module.exports = EventController;
